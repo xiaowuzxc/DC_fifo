659d5765a3aed22f52cd65ef387a07d6b3577de5
xiaowuzxc <wuyan-100@163.com>
2021-12-25 14:50:46 +0800

    makefile改动

--- testbench_iverilog+gtk/make/make.bat
+++ testbench_iverilog+gtk/make/make.bat
@@ -1,6 +1,6 @@
 chcp 65001
 echo "开始编译"
-iverilog -g2005-sv -o tb -y ..\module ..\tb\tb_fifo_async.sv
+iverilog -g2005-sv -o tb -y ..\.. ..\tb\tb_fifo_async.sv
 echo "生成波形"
 vvp -n tb -lxt2
 echo "显示波形"
